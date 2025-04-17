981a7d26b826ba5f6e2697090c688a5ee2a5fc42
Aman Vats <aman.vats@rtctek.com>
2025-04-17 12:27:35 +0530

    Add project files.

---
+++ SoftwareServerpp/Controllers/SoftwareDetailsAPI.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SoftwareServerpp.Models;
+using System.Text;
+
+namespace SoftwareServerpp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SoftwareDetailsAPI : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult GetSoftwareDetails([FromBody] List<SoftwareDetails> data)
+        {
+            if (data == null)
+            {
+                return BadRequest("Invalid Data");
+            }
+
+            var CsvBuilder = new StringBuilder();
+            CsvBuilder.AppendLine("DisplayName,DisplayVersion,Publisher,InstallDate,InstallLocation,EstimatedSize");
+            foreach (var software in data)
+            {
+                CsvBuilder.AppendLine($"{software.DisplayName},{software.DisplayVersion},{software.Publisher},{software.InstallDate},{software.InstallLocation},{software.EstimatedSize}");
+            }
+            var csvBytes = Encoding.UTF8.GetBytes(CsvBuilder.ToString());
+            var fileName = $"SoftwareDetails{DateTime.Now:yyyyMMdd_HHmmss}.csv";
+
+            return File(csvBytes, "text/csv", fileName);
+        }
+    }
+}
---
+++ SoftwareServerpp/Models/SoftwareDetails.cs
@@ -0,0 +1,13 @@
+﻿
+namespace SoftwareServerpp.Models
+{
+    public class SoftwareDetails
+    {
+        public string DisplayName { get; set; }
+        public string DisplayVersion { get; set; }
+        public string Publisher { get; set; }
+        public string InstallDate { get; set; } // e.g. "20240401"
+        public string InstallLocation { get; set; }
+        public double EstimatedSize { get; set; } // optional
+    }
+}
